0481386cf85463aee1e8c60f1f8f105ca0fb71b0
Andrew Wang <wangandrw330@gmail.com>
2025-12-28 01:42:21 -0500

    fix gitignore

--- backend/app/create_app.py
+++ backend/app/create_app.py
@@ -6,9 +6,14 @@ from app.config import Config
 from app.extensions import db, migrate
 from app.routes.health import health_bp
 from app import models
+from flask_cors import CORS
 
 def create_app():
   app = Flask(__name__)
+  CORS(app, origins=[
+        "http://localhost:5173",
+        "https://your-frontend.vercel.app"
+  ])
   app.config.from_object(Config)
 
   db.init_app(app)
